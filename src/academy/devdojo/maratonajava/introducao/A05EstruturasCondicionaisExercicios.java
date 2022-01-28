f3fd48fe3d63ebb883357dd5179b57458df9e28e
Elvis Muniz <mnz.pessoal@gmail.com>
2022-01-28 20:08:30 -0300

    Estruturas Condicionais Exercicios

---
+++ src/academy/devdojo/maratonajava/introducao/A05EstruturasCondicionaisExercicios.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class A05EstruturasCondicionaisExercicios {
+    public static void main(String[] args) {
+
+        double salarioAnual = 25000;
+        double primeiraFaixa = 9.78 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.58 / 100;
+        double valorImposto;
+
+        if (salarioAnual <= 34712) {
+            valorImposto = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual >= 34713 && salarioAnual <= 68587) {
+            valorImposto = salarioAnual * segundaFaixa;
+        } else {
+            valorImposto = salarioAnual * terceiraFaixa;
+        }
+
+        System.out.println("Ganhando "+salarioAnual+" por ano, vocâ paga de imposto: "+valorImposto);
+    }
+}
