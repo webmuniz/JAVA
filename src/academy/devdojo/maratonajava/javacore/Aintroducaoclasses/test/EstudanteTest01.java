562066e404284810555be26d728a526c48120d2b
Elvis Muniz <mnz.pessoal@gmail.com>
2022-01-29 12:06:48 -0300

    Java OO - Introducao Classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Elvis";
+        estudante.idade = 30;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
