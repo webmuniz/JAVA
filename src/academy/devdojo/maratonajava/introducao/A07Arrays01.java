b7d558f4a03fc5ffc3d43a620708a8a79853e54d
Elvis Muniz <mnz.pessoal@gmail.com>
2022-01-29 10:25:03 -0300

    Arrays

---
+++ src/academy/devdojo/maratonajava/introducao/A07Arrays01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.lang.reflect.Array;
+
+public class A07Arrays01 {
+    public static void main(String[] args) {
+
+        int [] idades = new int[3];
+
+        idades[0] = 21;
+        idades[1] = 12;
+        idades[2] = 45;
+
+        System.out.println(idades[2]);
+
+
+    }
+}
