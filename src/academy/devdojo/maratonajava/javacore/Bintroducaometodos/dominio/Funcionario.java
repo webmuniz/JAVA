decd1151e8087e829d8e140637c05793e47e7a37
Elvis Muniz <mnz.pessoal@gmail.com>
2022-01-30 13:07:48 -0300

    Java OO - Modificadores de Acesso

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,15 +1,15 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void mediaSalarios() {
         if (salarios == null){
             return;
         }
-        double media = 0;
         for (double salario : salarios) {
             media += salario;
         }
@@ -31,4 +31,32 @@ public class Funcionario {
         System.out.println("\n=-=-=-=-=-=-==-=-=-=-=-=-=");
     }
 
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -6,9 +6,11 @@ public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario1 = new Funcionario();
 
-        funcionario1.nome = "Elvis";
-        funcionario1.idade = 30;
-        funcionario1.salarios = new double[]{1200, 987.32, 2000};
+        funcionario1.setNome("Elvis");
+        funcionario1.setIdade(30);
+        funcionario1.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario1.imprime();
+
+        System.out.println("Média: "+funcionario1.getMedia());
     }
 }
