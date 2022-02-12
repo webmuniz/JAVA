23d6c1c1c4061deabc2476b0f0a7588e7e988e4f
Elvis Muniz <mnz.pessoal@gmail.com>
2022-02-12 19:41:41 -0300

    Innner Classes - Introduction

---
+++ src/academy/devdojo/maratonajava/javacore/innerclasses/test/InnerClassesTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.innerclasses.test;
+
+public class InnerClassesTest01 {
+    private String name = "Monkey D. Luffy";
+    
+    class Inner{
+        public void printOuterClassAttribute(){
+            System.out.println(name);
+            System.out.println(this);//Intern object
+            System.out.println(InnerClassesTest01.this);//Extern object
+        }
+    }
+    
+    public static void main(String[] args) {
+        InnerClassesTest01 innerClass = new InnerClassesTest01();
+        Inner inner = innerClass.new Inner();
+        Inner inner2 = new InnerClassesTest01().new Inner();
+
+        inner.printOuterClassAttribute();
+        inner2.printOuterClassAttribute();
+    }
+}
