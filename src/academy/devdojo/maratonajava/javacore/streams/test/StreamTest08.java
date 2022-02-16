0620b6a7401a8b1eea7ff09bcac1873dc97a8ba1
Elvis Muniz <mnz.pessoal@gmail.com>
2022-02-16 13:48:52 -0300

    Stream - Reduce

---
+++ src/academy/devdojo/maratonajava/javacore/streams/test/StreamTest08.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.streams.test;
+
+import academy.devdojo.maratonajava.javacore.streams.domain.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest08 {
+
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Death Note", 3.99),
+            new LightNovel("Dragon Ball", 2.59),
+            new LightNovel("Demon Slayer", 5.89),
+            new LightNovel("Demon Slayer", 5.89),
+            new LightNovel("Pokemon", 1.99),
+            new LightNovel("Fullmetal alchemist", 5.99),
+            new LightNovel("Monogatari", 1.60),
+            new LightNovel("Overlord", 8.99)
+    ));
+
+    public static void main(String[] args) {
+
+        lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println);
+
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+        System.out.println(sum);
+
+    }
+
+
+
+}
